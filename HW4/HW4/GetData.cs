e7b9111f45c31731908db1e9a10556c47c707afd
unknown <YKHODOS@dp337.softservecom.com>
2014-07-21 18:56:09 +0300

    from work

---
+++ HW4/HW4/GetData.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HW3;
+
+namespace HW4
+{
+    public class GetData
+    {
+        // get name 
+        static public string Name()
+        {
+            string name = "";
+            bool isEntered = false;
+            while (!isEntered)
+            {
+                Console.WriteLine("Name:");
+                name = Console.ReadLine();
+                if (name.Length > 0)
+                {
+                    isEntered = true;
+                }
+            }
+            return name;
+        }
+
+        //get date 
+        static public DateTime Date()
+        {
+            DateTime isDate;
+            Console.WriteLine("Date of birth:");
+            while (!DateTime.TryParse(Console.ReadLine(), out isDate))
+            {
+                Console.WriteLine("Invalid date. Format is mm.dd.yyyy");
+            }
+            return isDate;
+        }
+
+        //get Profile
+        static public string Profile()
+        {
+            string profile = "";
+            bool isEntered = false;
+            while (!isEntered)
+            {
+                Console.WriteLine("Profile:");
+                profile = Console.ReadLine();
+                if (profile.Length > 0)
+                {
+                    isEntered = true;
+                }
+            }
+            return profile;
+
+
+        }
+
+        //base salary
+        static public float Salary()
+        {
+            float salary;
+            Console.WriteLine("Base Salary($): ");
+            while (!Single.TryParse(Console.ReadLine(), out salary))
+            {
+                Console.WriteLine("Please, enter float value:");
+            }
+            return salary;
+        }    
+
+    }
+}
---
+++ HW4/HW4/XMLfileActions.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+using System.Xml.Serialization;
+using System.IO;
+
+namespace HW4
+{
+    
+        public class  XMLfileActions<T>
+        {
+            Type dataType;
+            string filePath;
+
+            public void XMLFile(string filePath)
+            {
+                this.filePath = filePath + ".xml";
+                dataType = typeof(T);
+            }
+
+            public T Load()
+            {
+                TextReader tr = new StreamReader("ListOfWorkers.xml");
+                XmlSerializer reader = new XmlSerializer(dataType);
+                T instance = (T)reader.Deserialize(tr);
+                tr.Close();
+                return instance;
+            }
+
+            public void Save(object Obj)
+            {
+                TextWriter tw = new StreamWriter(filePath);
+                XmlSerializer writer = new XmlSerializer(dataType);
+                writer.Serialize(tw, Obj);
+                tw.Close();
+            }
+
+        }
+    
+}
