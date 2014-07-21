c7996dc8e96d3425daabe7745a1f1a7bf6791a32
unknown <YKHODOS@dp337.softservecom.com>
2014-07-21 20:15:14 +0300

    from work 2. switch problems

---
+++ HW4/HW4/xmlProcessing.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Xml;
+using System.Xml.Serialization;
+
+namespace HW4
+{
+    class xmlProcessing<T>
+    {
+        Type dataType;
+        string filePath;
+
+        public xmlProcessing(string filePath)
+        {
+            this.filePath = filePath + ".xml";
+            dataType = typeof( T );
+         }
+
+        public T Load()
+        {
+            TextReader tr = new StreamReader(filePath);
+            XmlSerializer reader = new XmlSerializer(dataType);
+            T instance = (T)reader.Deserialize(tr);
+            tr.Close();
+            return instance;
+        }
+
+        public void Save(object Obj)
+        {
+            TextWriter tw = new StreamWriter(filePath);
+            XmlSerializer writer = new XmlSerializer(dataType);
+            writer.Serialize(tw, Obj);
+            tw.Close();
+        }
+    }
+}
