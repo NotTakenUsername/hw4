89c9d477b21e32e6c46e56073cc7052ca6843844
unknown <YKHODOS@dp337.softservecom.com>
2014-07-22 11:47:42 +0300

    tried to add dictionary

---
+++ HW4/HW4/Decision.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW4
+{
+    class Decision
+    {
+        static public bool AddOrQuit(bool AddWorker)
+        {
+            bool correctKey = false;
+                       
+            while (!correctKey)
+            {
+                ConsoleKeyInfo choise = Console.ReadKey();
+                if (choise.KeyChar == 'y' || choise.KeyChar == 'Y')
+                {
+                    AddWorker = true;
+                    correctKey = true;
+                }
+                else if (choise.KeyChar == 'n' || choise.KeyChar == 'N')
+                {
+                    AddWorker = false;
+                    correctKey = true;
+                }
+                else
+                {
+                    Console.WriteLine("Incorrect input.Press Y to add new record. Press N to quit:");
+                    correctKey = false;
+                }
+            }
+
+            return AddWorker;
+
+        }
+    }
+}
